ca785094a37e8234005adfd0004ab8c357c218ac
Dongyoung Kim <romankim.mit@gmail.com>
2021-03-29 14:49:07 -0500

    init

---
+++ deeplearning/push.sh
@@ -0,0 +1,5 @@
+#docker build -t deeplearning .
+#docker tag deeplearning leafclone/deeplearning:1
+#docker push leafclone/deeplearning:1
+docker build -t leafclone/deeplearning .
+docker push leafclone/deeplearning
